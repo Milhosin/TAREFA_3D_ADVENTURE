fc05b19ee4af2918687d36e79280ce700b0466f9
milhosin <milho.of@gmail.com>
2026-07-08 18:25:49 -0300

    Update (Enemy)
    Descrição:
    
    O jogo possui dois inimigos, um grande e um pequeno.
    
    Os inimigos são afetados pelos tiros da gun, ou seja, eles tem vida e podem ser derrotados.
    
    Quando o dano ocorre o inimigo pisca com uma cor diferente da dele original, para identificar o dano causado.
    
    Dois imigos, possuem diferença de tamanho, sendo o menor com 10 de vida e o maior com 20 de vida.
    
    O inimigo possui animações: IDLE, RUN, DEATH e ATTACK. Mas por agora só foi usado 2, a IDLE (parado) e a DEATH (morrendo).
    
    Ao derrotar os inimigos eles somem depois de uns 3 segundos.

---
+++ Assets/Script/Enemies/EnemyBase.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Animation;
+
+namespace Enemy
+{
+    public class EnemyBase : MonoBehaviour, IDamageable
+    {
+        public Collider colliderNew;
+        public FlashColor flashColor;
+
+        public float startLife = 10f;
+
+        [SerializeField] private float _currentLife;
+
+        [Header("Animation")]
+        [SerializeField] private AnimationBase _animationBase;
+
+        [Header("Start Animation")]
+        public float startAnimationDuration = .2f;
+        public Ease startAnimationEase = Ease.OutBack;
+        public bool startWithBornAnimation = true;
+
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        protected void ResetLife()
+        {
+            _currentLife = startLife;
+        }
+
+        protected virtual void Init()
+        {
+            ResetLife();
+
+            if(startWithBornAnimation)
+                BornAnimation();
+        }
+
+
+        protected virtual void Kill()
+        {
+            OnKill();
+        }
+
+
+        protected virtual void OnKill()
+        {
+            if (colliderNew != null) colliderNew.enabled = false;
+            Destroy(gameObject, 3f);
+            PlayAnimationByTrigger(AnimationType.DEATH);
+        }
+
+        public void OnDamage(float f)
+        {
+            if (flashColor != null) flashColor.Flash();
+
+            _currentLife -= f;
+
+            if(_currentLife <= 0)
+            {
+                Kill();
+            }
+        }
+
+        #region ANIMATION
+        private void BornAnimation()
+        {
+            transform.DOScale(0, startAnimationDuration).SetEase(startAnimationEase).From();
+        }
+
+
+        public void PlayAnimationByTrigger(AnimationType animationType)
+        {
+            _animationBase.PlayAnimationByTrigger(animationType);
+        }
+        #endregion
+
+        //debug
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                OnDamage(5f);            }
+        }
+
+        public void Damage(float damage)
+        {
+            Debug.Log("Damage");
+            OnDamage(damage);
+        }
+    }
+}
